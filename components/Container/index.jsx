941434f4c75edbf24578117eb501d2713d30623e
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-09 18:29:16 +0100

    add props to container

--- components/Container/index.jsx
+++ components/Container/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from './container.module.css';
 
-export const Container = ({ children, fullWidth }) => {
+export const Container = ({
+  children,
+  fullWidth,
+  className = '',
+  ...props
+}) => {
   return (
     <div
-      className={`wrapper ${fullWidth ? styles.fullWidth : styles.standard}`}
+      {...props}
+      className={`wrapper ${className} ${
+        fullWidth ? styles.fullWidth : styles.standard
+      }`}
     >
       {children}
     </div>
