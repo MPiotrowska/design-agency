9d85ed25dd6d3d77446e3d1b901d3ae03b2f8341
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-12 21:42:04 +0100

    create list component

--- components/Column/index.jsx
+++ components/Column/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './column.module.css';
 
-export const Column = ({ children }) => {
+export const List = ({ children }) => {
   return <div className={styles.column}>{children}</div>;
 };
