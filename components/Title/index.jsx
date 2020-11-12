4f407b9ccac9a678dcaea93693c0f3a382b288c5
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-12 21:45:45 +0100

    titlle logic

--- components/Title/index.jsx
+++ components/Title/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './title.module.css';
 
-export const Title = ({ children }) => {
-  return <h1 className={styles.title}>{children}</h1>;
+export const Title = ({ children, white }) => {
+  return (
+    <h1 className={`${white ? styles.white : styles.black}`}>{children}</h1>
+  );
 };
