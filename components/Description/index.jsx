daa0d3e0bb665773eb6fca904c53710c559435c5
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-12 21:43:38 +0100

    add description logig

--- components/Description/index.jsx
+++ components/Description/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './description.module.css';
 
-export const Description = ({ children }) => {
-  return <p className={styles.description}>{children}</p>;
+export const Description = ({ children, white }) => {
+  return <p className={`${white ? styles.white : styles.black}`}>{children}</p>;
 };
