89c041f49c9a3c6c0fbe27dcbb65ad8544cfe4f7
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-14 10:34:18 +0100

    add new component

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,16 +6,18 @@ import { Container } from '../components/Container';
 import { Footer } from '../components/Footer';
 import { NavBar } from '../components/NavBar';
 import { FlexBox } from '../components/FlexBox';
-// styles
-import styles from '../styles/Home.module.css';
 import { Description } from '../components/Description';
-import containerStyles from '../components/Container/container.module.css';
 import { Title } from '../components/Title';
 import { Section } from '../components/Section';
 import { List } from '../components/List';
-import listStyles from '../components/List/list.module.css';
 import { MiniCard } from '../components/MiniCard';
 import { ContactUs } from '../components/ContactUs';
+import { FirmDescription } from '../components/FirmDescription';
+
+// styles
+import styles from '../styles/Home.module.css';
+import containerStyles from '../components/Container/container.module.css';
+import listStyles from '../components/List/list.module.css';
 
 export default function Home() {
   return (
@@ -31,9 +33,10 @@ export default function Home() {
           <Container>
             <Title>Faster sites get more traffic and make more money.</Title>
             <Description>
-              Logo is a design and strategy studio based in Barcelona. We build
-              transformative digital products & brands. We work with processes
-              clients treasure.
+              Slow sites are the bane of the internet. We are JAMstack experts
+              that use Gatsby to build blazing-fast sites that are a pleasure to
+              use and a joy to behold. Maximize your business potential with a
+              static website today.
             </Description>
             <Link href="/contact">
               <a className={styles.buttonLink}>Contact</a>
@@ -41,50 +44,61 @@ export default function Home() {
           </Container>
         </Container>
       </Container>
-      <main className={styles.main}>
+      <Container fullWidth>
         <Container>
-          <FlexBox justifyContent="space-around">
-            <Card
-              style={{ flex: 1, marginRight: '20px' }}
-              caseLink="https://google.com"
-              caseTitle="Samsung"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              asperiores expedita debitis nihil odio? Sunt quae impedit
-              assumenda nisi, id quos porro optio animi natus fugiat maiores
-              culpa alias quaerat!
-            </Card>
-            <Card
-              style={{ flex: 1, marginRight: '20px' }}
-              caseLink="https://google.com"
-              caseTitle="Samsung"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut
-              quibusdam error repellendus omnis dolor quas quisquam in quasi.
-              Itaque vero amet eaque? Rem officia aliquam iusto unde quisquam
-              necessitatibus.
-            </Card>
-            <Card
-              style={{ flex: 1, marginRight: '20px' }}
-              caseLink="https://google.com"
-              caseTitle="Samsung"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut
-              quibusdam error repellendus omnis dolor quas quisquam in quasi.
-              Itaque vero amet eaque? Rem officia aliquam iusto unde quisquam
-              necessitatibus.
-            </Card>
-            <Card
-              style={{ flex: 1, marginRight: '20px' }}
-              caseLink="https://google.com"
-              caseTitle="Samsung"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut
-              quibusdam error repellendus omnis dolor quas quisquam in quasi.
-              Itaque vero amet eaque? Rem officia aliquam iusto unde quisquam
-              necessitatibus.
-            </Card>
-          </FlexBox>
+          <FirmDescription>
+            Logo is a design and strategy studio based in Barcelona. We build
+            transformative digital products & brands. We work with processes
+            clients treasure.
+          </FirmDescription>
+        </Container>
+      </Container>
+      <main className={styles.main}>
+        <Container fullWidth>
+          <Container>
+            <FlexBox justifyContent="space-around">
+              <Card
+                style={{ flex: 1, marginRight: '20px' }}
+                caseLink="https://google.com"
+                caseTitle="Samsung"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                asperiores expedita debitis nihil odio? Sunt quae impedit
+                assumenda nisi, id quos porro optio animi natus fugiat maiores
+                culpa alias quaerat!
+              </Card>
+              <Card
+                style={{ flex: 1, marginRight: '20px' }}
+                caseLink="https://google.com"
+                caseTitle="Samsung"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                ut quibusdam error repellendus omnis dolor quas quisquam in
+                quasi. Itaque vero amet eaque? Rem officia aliquam iusto unde
+                quisquam necessitatibus.
+              </Card>
+              <Card
+                style={{ flex: 1, marginRight: '20px' }}
+                caseLink="https://google.com"
+                caseTitle="Samsung"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                ut quibusdam error repellendus omnis dolor quas quisquam in
+                quasi. Itaque vero amet eaque? Rem officia aliquam iusto unde
+                quisquam necessitatibus.
+              </Card>
+              <Card
+                style={{ flex: 1, marginRight: '20px' }}
+                caseLink="https://google.com"
+                caseTitle="Samsung"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                ut quibusdam error repellendus omnis dolor quas quisquam in
+                quasi. Itaque vero amet eaque? Rem officia aliquam iusto unde
+                quisquam necessitatibus.
+              </Card>
+            </FlexBox>
+          </Container>
         </Container>
         <Container fullWidth style={{ backgroundColor: '#102990' }}>
           <Container style={{ backgroundColor: '#102990' }}>
@@ -143,10 +157,7 @@ export default function Home() {
           </Container>
         </Container>
         <Container fullWidth style={{ backgroundColor: '#102990' }}>
-          <ContactUs
-            caseLink="https://google.com"
-            caseTitle="Let’s talk. Contact Us."
-          />
+          <ContactUs caseLink="/contact" caseTitle="Let’s talk. Contact Us." />
         </Container>
       </main>
       <Footer />
