3287183138a0ce31fa8ab37970d8934f8c886eb3
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-15 19:51:41 +0100

    contact page

---
+++ components/FormImage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './formImage.module.css';
+
+const testImage =
+  'https://via.placeholder.com/550x700/bebfde/000000%20?Text=Digital.com%20C/O%20https://placeholder.com/';
+
+export const FormImage = ({ src = testImage, alt = '' }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Container } from '../components/Container';
+import { FlexBox } from '../components/FlexBox';
+
+import { Form } from '../components/Form';
+import { FormImage } from '../components/FormImage';
+import { NavBar } from '../components/NavBar';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <NavBar />
+      <Container fullWidth>
+        <Container>
+          <FlexBox>
+            <Form />
+            <FormImage />
+          </FlexBox>
+        </Container>
+      </Container>
+    </div>
+  );
 };
 
 export default Contact;
