213832324c3a7a6c3440825de761007bea3b7a6d
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-16 21:24:42 +0100

    add contact form styles

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,24 +1,79 @@
 import React from 'react';
+import styles from './form.module.css';
 
 export const Form = () => {
   return (
     <div>
+      <div>
+        <h1 className={styles.title}>Let&apos;s work together</h1>
+
+        <p className={styles.info}>Or email us at</p>
+      </div>
       <form name="contact" action="/success" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label htmlFor="yourname">Your Name:</label> <br />
-          <input type="text" name="name" id="yourname" />
+          <label className={styles.label} htmlFor="name">
+            Name:
+          </label>
+          <br />
+          <input
+            className={styles.formInput}
+            type="text"
+            name="name"
+            id="name"
+          />
+        </p>
+
+        <p>
+          <label className={styles.label} htmlFor="emailadress">
+            Email Adress:
+          </label>{' '}
+          <br />
+          <input
+            className={styles.formInput}
+            type="email"
+            name="email"
+            id="email"
+          />
         </p>
         <p>
-          <label htmlFor="youremail">Your Email:</label> <br />
-          <input type="email" name="email" id="youremail" />
+          <label className={styles.label} htmlFor="help">
+            What can we help you with?
+          </label>{' '}
+          <br />
+          <input
+            className={styles.formInput}
+            type="text"
+            name="help"
+            id="help"
+          />
         </p>
         <p>
-          <label htmlFor="yourmessage">Message:</label> <br />
-          <textarea name="message" id="yourmessage" />
+          <label className={styles.label} htmlFor="budget">
+            What sized budget are you working with?
+          </label>
+          <br />
+          <input
+            className={styles.formInput}
+            type="text"
+            name="budget"
+            id="budget"
+          />
+        </p>
+
+        <p>
+          <label className={styles.label} htmlFor="yourmessage">
+            Message:
+          </label>{' '}
+          <br />
+          <textarea
+            className={styles.messageInput}
+            name="message"
+            id="yourmessage"
+          />
         </p>
         <p>
-          <button type="submit">Send</button>
+          <button type="submit">Submit</button>
         </p>
       </form>
     </div>
