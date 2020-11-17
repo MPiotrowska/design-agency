46d576ad970e49b34be1bbc93acdbf6959569301
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-17 18:37:27 +0100

    add styles to miniCard

--- pages/contact.js
+++ pages/contact.js
@@ -5,16 +5,39 @@ import { FlexBox } from '../components/FlexBox';
 import { Form } from '../components/Form';
 import { FormImage } from '../components/FormImage';
 import { NavBar } from '../components/NavBar';
+import { Section } from '../components/Section';
+import { Description } from '../components/Description';
+import { Adress } from '../components/Adress';
 
 const Contact = () => {
   return (
     <div>
       <NavBar />
-      <Container fullWidth>
+      <Container fullWidth style={{ paddingTop: '100px' }}>
         <Container>
           <FlexBox justifyContent="space-around">
             <Form />
             <FormImage />
+          </FlexBox>
+        </Container>
+      </Container>
+      <Container fullWidth style={{ backgroundColor: '#102990' }}>
+        <Container>
+          <FlexBox justifyContent="space-around">
+            <Section>
+              <Adress white cityName="Barcelona">
+                <p>Passeig de Gràcia 6</p>
+                <p>Barcelona</p>
+                <p>Spain</p>
+              </Adress>
+            </Section>
+            <Section>
+              <Adress white cityName="Buenos Aires">
+                <p>Av. del Libertador 3 </p>
+                <p>Buenos Aires</p>
+                <p>Argentina</p>
+              </Adress>
+            </Section>
           </FlexBox>
         </Container>
       </Container>
