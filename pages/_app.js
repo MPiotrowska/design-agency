c68f0ba9a0bd6e4636f74a0011795ab5ea120dcc
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-19 17:57:44 +0100

    custom app to myApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css';
+import App, { Container } from 'next/app';
+import { Page } from '../components/Page';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+    return (
+      <Container>
+        <Page>
+          <Component />
+        </Page>
+      </Container>
+    );
+  }
 }
 
 export default MyApp;
+
+// import '../styles/globals.css';
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
