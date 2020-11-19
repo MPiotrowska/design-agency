864e56870fabf63303d0e08d0d026e323dcf6dfc
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-19 17:57:23 +0100

    add page component

---
+++ components/Page/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Footer } from '../Footer';
+import { Header } from '../Header';
+
+export const Page = ({ children }) => {
+  return (
+    <div>
+      <Header />
+
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
