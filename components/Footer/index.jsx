de73f04af1a98a51eaba600c626707da35d2b351
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-21 12:37:33 +0100

    add social links to footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -5,10 +5,22 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href="https://www.instagram.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      </a>
+      <a
+        href="https://www.linkedin.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      </a>
+      <a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">
         Powered by{' '}
         <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
       </a>
