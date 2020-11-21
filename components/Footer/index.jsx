82d1a557cd60c495c163ee7cf847f9733c1c847d
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-21 13:04:46 +0100

    add social icons

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -9,20 +9,29 @@ export const Footer = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <img
+          src="images/icon-instagram.svg"
+          alt="Vercel Logo"
+          className={styles.logo}
+        />
       </a>
       <a
         href="https://www.linkedin.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <img
+          src="images/icon-linkedin.svg"
+          alt="Vercel Logo"
+          className={styles.logo}
+        />
       </a>
       <a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <img
+          src="images/icon-dribbble.svg"
+          alt="Vercel Logo"
+          className={styles.logo}
+        />
       </a>
     </footer>
   );
