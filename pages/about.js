6e7a12ee4e561056bf052430a2e757cb7503b712
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-24 18:29:33 +0100

    refactor about

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,13 @@ import { Container } from '../components/Container';
 import { ContactUs } from '../components/ContactUs';
 
 import styles from '../styles/Home.module.css';
+import { AboutSection } from '../components/AboutSection';
 
 const About = () => {
   return (
     <main className={styles.main}>
       <Container fullWidth>
-        <Container>p</Container>
+        <AboutSection />
       </Container>
       <Container fullWidth style={{ backgroundColor: '#102990' }}>
         <ContactUs caseLink="/contact" caseTitle="Let’s talk. Contact Us." />
