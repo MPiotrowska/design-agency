8f3131ef4f60e2b07e784217c773e77dfa2a7093
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-24 17:14:07 +0100

    add contact us section to about

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Container } from '../components/Container';
+import { ContactUs } from '../components/ContactUs';
 
 import styles from '../styles/Home.module.css';
 
 const About = () => {
   return (
     <main className={styles.main}>
-      <Container />
+      <Container fullWidth>
+        <Container>p</Container>
+      </Container>
+      <Container fullWidth style={{ backgroundColor: '#102990' }}>
+        <ContactUs caseLink="/contact" caseTitle="Let’s talk. Contact Us." />
+      </Container>
     </main>
   );
 };
