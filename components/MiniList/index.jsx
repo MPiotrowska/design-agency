aaab52cfec4d0da1bf5ec5573b015f6a71000314
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-28 19:38:27 +0100

    made list dynamic

--- components/MiniList/index.jsx
+++ components/MiniList/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styles from './miniList.module.css';
 
-export const MiniList = ({ caseTitle, white, className = '' }) => {
+export const MiniList = ({ children, caseTitle, white, className = '' }) => {
   return (
     <div className={styles.listContainer}>
       <h1 className={`  ${className}  ${white ? styles.white : styles.black}`}>
         {caseTitle}
       </h1>
-      <ul>
-        <li>Coffee</li>
-        <li>Tea</li>
-        <li>Coca Cola</li>
+      <ul className={`  ${className}  ${white ? styles.white : styles.black}`}>
+        <li className={styles.listItem}>{children}</li>
       </ul>
     </div>
   );
