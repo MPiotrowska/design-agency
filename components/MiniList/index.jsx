42444ca00f3b3ec9c7363a874b649dfc463ccf3c
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-28 20:15:27 +0100

    add a tag to list

--- components/MiniList/index.jsx
+++ components/MiniList/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './miniList.module.css';
 
-export const MiniList = ({ children, caseTitle, white, className = '' }) => {
+export const MiniList = ({
+  children,
+  caseTitle,
+  caseLink,
+  white,
+  className = '',
+}) => {
   return (
     <div className={styles.listContainer}>
-      <h1 className={`  ${className}  ${white ? styles.white : styles.black}`}>
-        {caseTitle}
-      </h1>
+      <a target="blank" className={styles.caseLink} href={caseLink}>
+        <h1
+          className={`  ${className}  ${white ? styles.white : styles.black}`}
+        >
+          {caseTitle}
+        </h1>
+      </a>
+
       <ul className={`  ${className}  ${white ? styles.white : styles.black}`}>
         <li className={styles.listItem}>{children}</li>
       </ul>
