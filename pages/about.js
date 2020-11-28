059943022db16bcfc121fde809e43f002bb13453
MPiotrowska <monikapiotrowska27@gmail.com>
2020-11-28 17:36:31 +0100

    add how we do it h3

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import { ContactUs } from '../components/ContactUs';
 import { FlexBox } from '../components/FlexBox';
 import { Section } from '../components/Section';
 import { AboutSection } from '../components/AboutSection';
-import { List } from '../components/List';
+
 import styles from '../styles/Home.module.css';
 
 const About = () => {
@@ -15,17 +15,10 @@ const About = () => {
       </Container>
       <Container fullWidth style={{ backgroundColor: '#102990' }}>
         <Container style={{ backgroundColor: '#102990' }}>
-          <h3>hgjhg</h3>
+          <h3>How we do it</h3>
           <h1> bjkb</h1>
           <FlexBox justifyContent="space-between">
-            <Section>
-              <List>
-                <p>
-                  <strong>.</strong>
-                  Product Strategy
-                </p>
-              </List>
-            </Section>
+            <Section>ffh</Section>
             <Section>knkn</Section>
             <Section>knkn</Section>
           </FlexBox>
