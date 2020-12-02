039ae62df10a4304e5250031d0c18bec1dde145f
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-02 17:36:57 +0100

    add new component

---
+++ components/Methodologies/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Container } from '../Container';
+import { FlexBox } from '../FlexBox';
+import { MiniList } from '../MiniList';
+import styles from './methodologies.module.css';
+
+const testImage =
+  'https://via.placeholder.com/600x450/bebfde/000000%20?Text=Digital.com%20C/O%20https://placeholder.com/';
+
+export const Methodologies = ({
+  src = testImage,
+  alt = '',
+
+  caseTitle,
+}) => {
+  return (
+    <Container fullWidth>
+      <Container>
+        <FlexBox justifyContent="space-between">
+          <img src={src} alt={alt} />
+
+          <div className={styles.listContainer}>
+            <p>OUR METHODOLOGIES</p>
+            <h2 className={styles.caseTitle}>Sprints and Lean UX</h2>
+            <MiniList>
+              {' '}
+              <ul>
+                <a
+                  href="https://google.com"
+                  className={styles.caseLink}
+                  target="blank"
+                >
+                  <li>
+                    ● Validate any idea in just 1-week with Design Sprint →
+                  </li>
+                </a>
+                <a
+                  href="https://google.com"
+                  target="blank"
+                  className={styles.caseLink}
+                >
+                  <li>
+                    ● Design great products with agile teams with Lean UX →
+                  </li>
+                </a>
+              </ul>
+            </MiniList>
+          </div>
+        </FlexBox>
+      </Container>
+    </Container>
+  );
+};
