4375934a765a4361516e5c874c90b256c6f1805f
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-08 22:00:02 +0100

    refactor flexbox in Clients comp by adding map

--- components/Clients/index.jsx
+++ components/Clients/index.jsx
@@ -3,6 +3,7 @@ import { Container } from '../Container';
 import { FlexBox } from '../FlexBox';
 import { Section } from '../Section';
 import styles from './clients.module.css';
+import data from '../../mocks/data.json';
 
 export const Clients = () => {
   return (
@@ -13,35 +14,22 @@ export const Clients = () => {
           These are some of our most renowned clients worldwide.
         </h1>
         <FlexBox justifyContent="space-between">
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
+          {data.images.map((image) => (
+            <Section>
+              <img src={image.url} alt={image.altText} />
+            </Section>
+          ))}
         </FlexBox>
         <FlexBox justifyContent="space-between">
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
-          <Section>
-            <img src="/images/blue.svg" alt="logo" />
-          </Section>
+          {data.logos.map((logo) => (
+            <Section>
+              <img width={200} height={80} src={logo.url} alt={logo.altText} />
+            </Section>
+          ))}
         </FlexBox>
+
         <Section maxWidth="100%" display="flex" justifyContent="center">
-          <img src="/images/best-year.svg" alt="logo" />
+          <img src={data.award.url} alt={data.award.altText} />
         </Section>
       </Container>
     </Container>
