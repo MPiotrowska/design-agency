cafd9fbea0e94cd4bd99836ba1a93c8edcf246bf
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-10 19:58:06 +0100

    add styles to flexBoxes

--- components/Clients/index.jsx
+++ components/Clients/index.jsx
@@ -4,6 +4,7 @@ import { FlexBox } from '../FlexBox';
 import { Section } from '../Section';
 import styles from './clients.module.css';
 import data from '../../mocks/data.json';
+import { Carousel } from '../Carousel';
 
 export const Clients = () => {
   return (
@@ -13,13 +14,15 @@ export const Clients = () => {
           {' '}
           These are some of our most renowned clients worldwide.
         </h1>
-        <FlexBox justifyContent="space-between">
-          {data.images.map((image) => (
-            <Section>
-              <img src={image.url} alt={image.altText} />
-            </Section>
-          ))}
-        </FlexBox>
+        <div className={styles.flexContainer}>
+          <FlexBox justifyContent="space-between">
+            {data.images.map((image) => (
+              <Section>
+                <img src={image.url} alt={image.altText} />
+              </Section>
+            ))}
+          </FlexBox>
+        </div>
         <FlexBox justifyContent="space-between">
           {data.logos.map((logo) => (
             <Section>
@@ -29,7 +32,19 @@ export const Clients = () => {
         </FlexBox>
 
         <Section maxWidth="100%" display="flex" justifyContent="center">
-          <img src={data.award.url} alt={data.award.altText} />
+          <img
+            className={styles.award}
+            src={data.award.url}
+            alt={data.award.altText}
+          />
+        </Section>
+        <Carousel />
+        <Section maxWidth="100%" display="flex" justifyContent="center">
+          <img
+            className={styles.rating}
+            src={data.rating.url}
+            alt={data.rating.altText}
+          />
         </Section>
       </Container>
     </Container>
