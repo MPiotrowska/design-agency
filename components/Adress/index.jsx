8d07566ac82d561e3500ffe88a456caaa72b9a0c
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-17 23:11:00 +0100

    refactor adress module

--- components/Adress/index.jsx
+++ components/Adress/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import styles from './adress.module.css';
+import data from '../../mocks/data.json';
+import { Section } from '../Section';
+import { FlexBox } from '../FlexBox';
 
-export const Adress = ({ children, cityName }) => {
+export const Adress = () => {
   return (
-    <div className={styles.adressContainer}>
-      <h3 className={styles.cityName}>{cityName}</h3>
-      <p className={styles.adressLine}>{children}</p>
-    </div>
+    <FlexBox justifyContent="space-around">
+      <Section>
+        {' '}
+        <div className={styles.adressContainer}>
+          <h3 className={styles.cityName}>{data.city.name}</h3>
+          {data.adress.map((item) => (
+            <p className={styles.adressLine}>{item.line}</p>
+          ))}
+        </div>
+      </Section>
+      <Section>
+        {' '}
+        <div className={styles.adressContainer}>
+          <h3 className={styles.cityName}>{data.cityBuenos.name}</h3>
+          {data.adressBuenos.map((item) => (
+            <p className={styles.adressLine}>{item.line}</p>
+          ))}
+        </div>
+      </Section>
+    </FlexBox>
   );
 };
