16ea9a4aba747c89fc02efbb194ad45f65102402
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-17 23:15:41 +0100

    refactor title

--- components/Title/index.jsx
+++ components/Title/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Description } from '../Description';
+import { Section } from '../Section';
 import styles from './title.module.css';
+import data from '../../mocks/data.json';
 
-export const Title = ({ children, white }) => {
+export const Title = () => {
   return (
-    <h1 className={`${white ? styles.white : styles.black}`}>{children}</h1>
+    <Section>
+      <h1 className={styles.white}>{data.designTitle.content}</h1>
+      <Description white />
+    </Section>
   );
 };
