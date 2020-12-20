e8fb0625f0b9849ceae70fc5a80c47a57a6146b7
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-20 21:22:28 +0100

    refactor clients component

--- components/Clients/index.jsx
+++ components/Clients/index.jsx
@@ -11,11 +11,13 @@ export const Clients = () => {
     <Container fullWidth style={{ backgroundColor: '#f6f7fb' }}>
       <Container>
         <h1 className={styles.purpose}>
-          {' '}
           These are some of our most renowned clients worldwide.
         </h1>
         <div className={styles.flexContainer}>
-          <FlexBox justifyContent="space-between">
+          <FlexBox
+            className={styles.logosContainer}
+            justifyContent="space-between"
+          >
             {data.images.map((image) => (
               <Section>
                 <img src={image.url} alt={image.altText} />
@@ -23,7 +25,10 @@ export const Clients = () => {
             ))}
           </FlexBox>
         </div>
-        <FlexBox justifyContent="space-between">
+        <FlexBox
+          justifyContent="space-between"
+          className={styles.logosContainer}
+        >
           {data.logos.map((logo) => (
             <Section>
               <img width={200} height={80} src={logo.url} alt={logo.altText} />
