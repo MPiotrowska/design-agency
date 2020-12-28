f0cf6014bbfdc1d2e96b133b6c7bac18b571905a
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-28 20:51:09 +0100

    refactor  form image component

--- components/FormImage/index.jsx
+++ components/FormImage/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './formImage.module.css';
+import data from '../../mocks/data.json';
 
-const testImage =
-  'https://via.placeholder.com/550x800/bebfde/000000%20?Text=Digital.com%20C/O%20https://placeholder.com/';
-
-export const FormImage = ({ src = testImage, alt = '' }) => {
+export const FormImage = ({ alt = '' }) => {
   return (
     <div className={styles.cardContainer}>
-      <img src={src} alt={alt} />
+      <img className={styles.image} src={data.contact.url} alt={alt} />
     </div>
   );
 };
