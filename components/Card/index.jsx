15fe881762018ae325d63921bbdfa1a432bf7040
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-09 14:21:12 +0100

    fix card

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -10,8 +10,14 @@ export const Card = () => {
       <Container>
         <FlexBox justifyContent="space-around">
           {data.cards.map((card) => (
-            <div key={card.title} className={styles.cardContainer}>
-              <img className={styles.image} src={card.url} alt={card.altText} />
+            <div key={card.title}>
+              <div className={styles.cardContainer}>
+                <img
+                  className={styles.image}
+                  src={card.url}
+                  alt={card.altText}
+                />
+              </div>
               <a target="blank" className={styles.caseLink} href={card.link}>
                 <h3 className={styles.caseTitle}>{card.title}</h3>
               </a>
